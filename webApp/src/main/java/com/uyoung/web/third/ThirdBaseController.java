7bc1dc394f8ff28a3228dbe586f469f702a12204
ylzhu <ylzhu@corp.netease.com>
2015-10-15 17:20:16 +0800

    第三方登录接入

---
+++ webApp/src/main/java/com/uyoung/web/third/ThirdBaseController.java
@@ -0,0 +1,39 @@
+package com.uyoung.web.third;
+
+import com.uyoung.core.base.service.HttpService;
+import com.uyoung.core.third.douban.constant.DouBanConstant;
+import org.apache.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Desc:
+ * <p/>Date: 2015-10-15
+ * <br/>Time: 16:25
+ * <br/>User: ylzhu
+ */
+@Controller
+public class ThirdBaseController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThirdBaseController.class);
+
+    @Autowired
+    private HttpService httpService;
+
+    @RequestMapping(value = "/getCode")
+    public String getAuthCode(String thirdType, HttpRequest request, HttpResponse response) {
+        httpService.sendGetRequest(DouBanConstant.getAuthUrl("/getToken", "http://www.baidu.com"));
+        return null;
+    }
+
+    @RequestMapping(value = "/getToken")
+    public String getAccessToken(String code, String redirectUrl) {
+        String tokenUrl = DouBanConstant.getTokenUrl(redirectUrl, code);
+        String accessTokeResult = httpService.sendPostRequest(tokenUrl);
+        LOGGER.info("#AccessTokenResult is " + accessTokeResult);
+        return null;
+    }
+}
---
+++ webApp/src/main/java/com/uyoung/web/third/interceptors/ThirdBaseInterceptor.java
@@ -0,0 +1,10 @@
+package com.uyoung.web.third.interceptors;
+
+/**
+ * Desc:
+ * <p/>Date: 2015-10-15
+ * <br/>Time: 16:36
+ * <br/>User: ylzhu
+ */
+public class ThirdBaseInterceptor {
+}
