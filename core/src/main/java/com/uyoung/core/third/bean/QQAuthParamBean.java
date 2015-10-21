d89bb7147803560b76e1ea02a1ae3d8a0817bb28
ylzhu <ylzhu@corp.netease.com>
2015-10-21 18:15:31 +0800

    增加QQ登录接入信息

---
+++ core/src/main/java/com/uyoung/core/third/bean/QQAuthParamBean.java
@@ -0,0 +1,39 @@
+package com.uyoung.core.third.bean;
+
+import com.uyoung.core.third.enums.ThirdPlatformEnum;
+
+/**
+ * Desc:
+ * <p/>Date: 2015-10-21
+ * <br/>Time: 18:13
+ * <br/>User: ylzhu
+ */
+public class QQAuthParamBean extends AuthParamBaseBean {
+    private static final String APP_KEY = "1104920700";
+    private static final String APP_SECRET = "KNuxx0WvgGddPBq6";
+
+    @Override
+    public String getAppKey() {
+        return null;
+    }
+
+    @Override
+    public String getAppSecret() {
+        return null;
+    }
+
+    @Override
+    public String getAuthBaseUrl() {
+        return null;
+    }
+
+    @Override
+    public String getTokenBaseUrl() {
+        return null;
+    }
+
+    @Override
+    public ThirdPlatformEnum getThirdPlatform() {
+        return null;
+    }
+}
