3a1cf322eb9dae858fa8226363e81b2a213600ec
ylzhu <ylzhu@corp.netease.com>
2015-11-09 11:54:55 +0800

    活动增加接口

--- webApp/src/main/java/com/uyoung/web/controller/ActivityInfoController.java
+++ webApp/src/main/java/com/uyoung/web/controller/ActivityInfoController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -57,7 +58,7 @@ public class ActivityInfoController extends BaseController {
         return JsonUtil.getJsonString(baseResult);
     }
 
-    @RequestMapping(value = "/activity/add")
+    @RequestMapping(value = "/activity/add", method = RequestMethod.POST)
     @ResponseBody
     public String add(ActivityInfo activityInfo) {
         if (activityInfo == null) {
