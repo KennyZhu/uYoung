6da95fba042b249daa52c58067a9cf37a90248ae
ylzhu <ylzhu@corp.netease.com>
2015-12-07 18:19:19 +0800

    增加Session表

---
+++ core/src/main/java/com/uyoung/core/api/model/Session.java
@@ -0,0 +1,66 @@
+package com.uyoung.core.api.model;
+
+import java.util.Date;
+
+/**
+ * Desc:
+ * <p/>Date: 2015-12-07
+ * <br/>Time: 17:34
+ * <br/>User: ylzhu
+ */
+public class Session {
+    private Integer id;
+    private Integer uid;
+    private String sessionId;
+    private Integer status;
+    private Date createTime;
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
