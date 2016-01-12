b2f1ea5e69f01d501f07edd70f5f957ba61c4ab6
KennyZhu <yanlongzhu2010@gmail.com>
2016-01-12 16:57:25 +0800

    增加登录接口

--- webApp/src/main/java/com/uyoung/web/controller/base/AlbumBaseController.java
+++ webApp/src/main/java/com/uyoung/web/controller/base/AlbumBaseController.java
@@ -1,9 +1,6 @@
 package com.uyoung.web.controller.base;
 
-import com.uyoung.core.api.exception.BusinessException;
-import com.uyoung.core.api.model.AlbumInfo;
 import com.uyoung.core.api.service.AlbumInfoService;
-import com.uyoung.web.enums.ResultCodeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -24,17 +21,17 @@ public class AlbumBaseController extends BaseController {
      * @return
      */
     protected boolean checkUser(Integer id) throws Exception {
-        Integer uid = getCurrentLoginUid();
-        if (uid == null) {
-            throw new BusinessException(ResultCodeEnum.NOT_LOGIN);
-        }
-        AlbumInfo albumInfo = albumInfoService.getById(id);
-        if (albumInfo == null) {
-            throw new BusinessException(ResultCodeEnum.ALBUM_NOT_FOUND);
-        }
-        if (!uid.equals(albumInfo.getCreateUserId())) {
-            throw new BusinessException(ResultCodeEnum.ALBUM_NOT_CREATOR);
-        }
+//        Integer uid = getCurrentLoginUid();
+//        if (uid == null) {
+//            throw new BusinessException(ResultCodeEnum.NOT_LOGIN);
+//        }
+//        AlbumInfo albumInfo = albumInfoService.getById(id);
+//        if (albumInfo == null) {
+//            throw new BusinessException(ResultCodeEnum.ALBUM_NOT_FOUND);
+//        }
+//        if (!uid.equals(albumInfo.getCreateUserId())) {
+//            throw new BusinessException(ResultCodeEnum.ALBUM_NOT_CREATOR);
+//        }
         return true;
     }
 }
