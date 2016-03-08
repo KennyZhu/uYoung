9eeefe115eed3ccacf126de49a7c7df6c5c891c4
ylzhu <ylzhu@corp.netease.com>
2016-03-08 10:41:30 +0800

    增加kafka配置

---
+++ core/src/main/java/com/uyoung/core/api/constant/KafkaConstant.java
@@ -0,0 +1,11 @@
+package com.uyoung.core.api.constant;
+
+/**
+ * Desc:
+ * <p/>Date: 2016-02-18
+ * <br/>Time: 17:43
+ * <br/>User: ylzhu
+ */
+public final class KafkaConstant {
+    public static final String CONFIG_DIR = "kafkaConfig";
+}
