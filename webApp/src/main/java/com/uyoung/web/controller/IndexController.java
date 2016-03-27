5070b11d32e03593a772554382e21c1ecd452f58
KennyZhu <yanlongzhu2010@gmail.com>
2016-03-27 20:45:07 +0800

    首页处理

--- webApp/src/main/java/com/uyoung/web/controller/IndexController.java
+++ webApp/src/main/java/com/uyoung/web/controller/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController extends BaseController {
 
-    @RequestMapping(value = "index.html")
+    @RequestMapping(value = "/index.html")
     public String index() {
         return "index.html";
     }
